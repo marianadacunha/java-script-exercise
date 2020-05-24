e7ce3a0071e22f121cea31be14304f216508f31e
marianadacunha <marianadacunhamarques@gmail.com>
2020-05-24 19:54:20 +0200

    discovering NaN

---
+++ operadores.js
@@ -0,0 +1,26 @@
+// multiplicação
+
+var operador = 500
+var operando = 10
+
+var resultado = operador * operando
+
+console.log(resultado)
+
+// adição
+
+var nome = 'Mariana'
+var sobrenome = 'Marques'
+
+var nomeCompleto = nome + " " + sobrenome
+
+console.log(nomeCompleto)
+
+// NaN
+
+var primeiroValor = 200
+var segundoValor = "amarelo"
+
+var subtracao = primeiroValor - segundoValor
+
+console.log(subtracao)
