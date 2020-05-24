4255baf28a9b1a6ee6e084534c1be78d6de30bec
marianadacunha <marianadacunhamarques@gmail.com>
2020-05-24 21:03:12 +0200

    using 'while' and 'for'

--- repeticao.js
+++ repeticao.js
@@ -7,8 +7,22 @@
 //    }
 //}
 
-var tabuada = 8;
+// var tabuada = 8;
 
-for (var i = 0; i <= 10; i ++){
-    console.log("Valor de " + tabuada + " x " + i + " = " + (tabuada * i));
+// for (var i = 0; i <= 10; i ++){
+//    console.log("Valor de " + tabuada + " x " + i + " = " + (tabuada * i));
+//}
+
+var achou = false;
+
+var numeroSorteado = 12;
+var possivelValor = 0;
+
+while (!achou) {
+    possivelValor += 1;
+        if (numeroSorteado === possivelValor) {
+            achou = true
+        } else {
+            console.log("Possível valor não corresponde ao número sorteado " + possivelValor);
+}
 }
