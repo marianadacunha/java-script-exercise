9b6fe028c8d859f3dd6ad9623bf0297997c0be0c
marianadacunha <marianadacunhamarques@gmail.com>
2020-05-24 20:43:40 +0200

    repetition exercise

---
+++ repeticao - part2.js
@@ -0,0 +1,5 @@
+var tabuada = 8;
+
+for (var i = 0; i <= 10; i ++){
+    console.log("Valor de " + tabuada + " x " + i + " = " + (tabuada * i));
+}
--- repeticao.js
+++ repeticao.js
@@ -1,5 +1,8 @@
 var numeroSorteado = 10;
 
-for (var i = 0; i < 100; i ++){
-
+for (var i = 0; i <= 100; i++){
+    if (numeroSorteado === i) {
+    console.log("Parabéns! Seu número foi encontrado!")
+    break;
+    }
 }
