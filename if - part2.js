8a37453d0c1267271f9947c87ddd257a34b6678a
marianadacunha <marianadacunhamarques@gmail.com>
2020-05-24 20:17:14 +0200

    more exercises with if

---
+++ if - part2.js
@@ -0,0 +1,13 @@
+var nome = "Mariana";
+
+switch (nome) {
+    case 'Mariana':
+        console.log('Boa!');
+        break;
+    case 'Marques':
+        console.log('Tá, pode ser também!');
+        break;
+    default: 
+        console.log('Não foi dessa vez...')
+        break;
+}
