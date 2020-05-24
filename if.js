27402ab812f364f0c59be27b1feee580fd029569
marianadacunha <marianadacunhamarques@gmail.com>
2020-05-24 20:11:05 +0200

    if exercise

---
+++ if.js
@@ -0,0 +1,11 @@
+var nome = "Maria"
+
+if (nome === "Mariana") {
+    console.log('Boa! Esse é o seu nome mesmo!');
+    } else if (nome === "Marina"){
+        console.log('Ok, está quase lá...');
+    } else {
+        console.log('Poxa, você não é a Mariana...');
+    }
+
+// = é atribuição ; === é pergunta
