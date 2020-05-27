43036333a53bc2dd604a02f98f835c107e26f267
marianadacunha <marianadacunhamarques@gmail.com>
2020-05-27 20:52:21 +0200

    template string

---
+++ template-string.js
@@ -0,0 +1,6 @@
+let name = 'Mariana';
+let myLastName = 'Marques';
+let myProfession = 'Front-end Developer';
+
+console.log('Hello, my name is ' + name + ' ' + myLastName + ' and I work as a ' + myProfession)
+console.log(`Hello, my name is ${name} ${myLastName} and I work as a ${myProfession}`)
