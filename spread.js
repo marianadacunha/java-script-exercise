461f19ecb9b3118239c058897e04ea6e79775cc4
marianadacunha <marianadacunhamarques@gmail.com>
2020-05-28 15:01:10 +0200

    spread new example

--- spread.js
+++ spread.js
@@ -1,16 +1,25 @@
 let list = [1, 2, 5];
 let addList = [3, 4];
 
-console.log(list)
-console.log(addList)
-
 for (let i = 0; i < addList.length; i++) {
-    for (let k = 0; k < list.length; i++) {
-        if(list[k] >= addList[i]) {
+    for (let k = 0; k < list.length; k++) {
+        if (list[k] >= addList[i]) {
             list.splice(k, 0, addList[i]);
             k++;
         }
     }
 }
 
-console.log(list)
+const listResult = [1, 2, ...addList, 5]
+
+console.log(list);
+
+// other example
+
+let array1 = ['a', 'b', 'c'];
+let array2 = [...array1];
+
+array2.push('d');
+
+console.log(`Array1 =${array1}`);
+console.log(`Array2 =${array2}`);
