c4f516df04acfcc255a20c91b21aa2000bcf975a
marianadacunha <marianadacunhamarques@gmail.com>
2020-06-06 14:13:31 +0200

    comparisons with the logical and operator

---
+++ &&.js
@@ -0,0 +1,15 @@
+// using if statement
+
+if (num > 5) {
+    if (num < 10) {
+      return "Yes";
+    }
+  }
+  return "No";
+
+// using &&
+
+if (num > 5 && num < 10) {
+    return "Yes";
+  }
+  return "No";
